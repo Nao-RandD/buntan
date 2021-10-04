285bd7cdd1eee3da959ca6d4d2b3feb110ac4a9c
Nao-RandD <nao.r.and.d@gmail.com>
2021-10-04 21:11:08 +0900

    ログイン処理を管理する機能を追加

--- buntan/Controller/SignupViewController.swift
+++ buntan/Controller/SignupViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import Firebase
 
 class SignupViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var houseGroupTextField: UITextField!
+    @IBOutlet weak var groupTextField: UITextField!
+
+    private let userDefaults = UserDefaults.standard
+    private var groupPickerView: UIPickerView!
+    /// FIXME - 仮置として追加
+    private var groupList = ["山本ハウス", "太田夫妻宅"]
+    private let db = Firestore.firestore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // groupPickerViewを設定
+        groupPickerView = UIPickerView()
+        groupPickerView.delegate = self
+        groupPickerView.dataSource = self
+        groupTextField.inputView = groupPickerView
 
-        // Do any additional setup after loading the view.
+        let collectionReference = db.collection("group")
+        collectionReference.getDocuments { (_snapshot, _error) in
+            if let error = _error {
+                print(error.localizedDescription)
+                return
+            }
+
+            let datas = _snapshot!.documents.compactMap { $0.data() }
+            let groups = datas.map {
+                $0["name"]
+            } as? [String]
+            self.groupList = groups ?? ["ハウス", "自宅"]
+            print(self.groupList)
+        }
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -20,8 +48,62 @@ class SignupViewController: UIViewController {
     }
 
     @IBAction func tappedSingupButton(_ sender: Any) {
+        guard let name = nameTextField.text, !name.isEmpty else {
+            showAlert(title: "必須項目の入力漏れ",
+                      message: "ユーザー名を入力してください")
+            return
+        }
+        guard let group = groupTextField.text, !group.isEmpty else {
+            showAlert(title: "必須項目の入力漏れ",
+                      message: "参加するグループを選択してください")
+            return
+        }
+
         let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "TabBarController") as! TabBarController
         secondViewController.modalPresentationStyle = .fullScreen
         self.present(secondViewController, animated: false, completion: nil)
+
+        userDefaults.set(name, forKey: "User")
+        userDefaults.set(group, forKey: "Group")
+
+        let data: [String: Any] = ["name": name, "group": group, "point": 0]
+        db.collection("users").document(name).setData(data, merge: true)
+        print("userDefaultsを更新")
+        print("ユーザー名：\(name), グループ：\(group)としてSign Up完了")
+    }
+}
+
+extension SignupViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message:  message,
+                                      preferredStyle:  UIAlertController.Style.alert)
+        let confirmAction = UIAlertAction(title: "OK",
+                                          style: UIAlertAction.Style.default,
+                                          handler: {
+                                            (action: UIAlertAction!) -> Void in
+        })
+        alert.addAction(confirmAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+/// MARK - UICollectionViewDelegate
+extension SignupViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        groupList.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let user = groupList[row]
+        groupTextField.text = user
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        groupList[row]
     }
 }
