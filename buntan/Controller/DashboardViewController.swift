c0a0cea080dabad9eacef3dd877e7af89712065b
Nao-RandD <nao.r.and.d@gmail.com>
2021-11-24 21:16:02 +0900

    ダッシュボードにポイント順でユーザーを表示するように追加

--- buntan/Controller/DashboardViewController.swift
+++ buntan/Controller/DashboardViewController.swift
@@ -6,10 +6,16 @@
 //
 
 import Foundation
+import Firebase
 import UIKit
 
 class DashboardViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+
+    private let db = Firestore.firestore()
+    private var taskListener: ListenerRegistration?
+    private let userDefaults = UserDefaults.standard
+
     private var userPointList: [UserInfo] = [
         UserInfo(name: "Shin", point: 20),
         UserInfo(name: "Nao", point: 40),
@@ -24,6 +30,7 @@ class DashboardViewController: UIViewController {
         tableView.register(UINib(nibName: "DashboardTableViewCell", bundle: nil),
                                    forCellReuseIdentifier: "DashboardCell")
 
+        setListener()
 //        let user1 = User(name: "Shin", point: 20)
 //        let user2 = User(name: "Nao", point: 40)
 //        let user3 = User(name: "Ryoya", point: 30)
@@ -32,7 +39,28 @@ class DashboardViewController: UIViewController {
 //        userPointList.append(user2)
 //        userPointList.append(user3)
     }
+}
 
+extension DashboardViewController {
+    private func setListener() {
+        self.taskListener = db.collection("users").addSnapshotListener { snapshot, e in
+                if let snapshot = snapshot {
+                    let group = self.userDefaults.object(forKey: "Group") as! String
+                    let tasks = snapshot.documents.filter { $0["group"] as! String == group }
+
+                    var i = 0
+                    for user in self.userPointList {
+                        for task in tasks {
+                            if task["name"] as! String == user.name {
+                                self.userPointList[i].point = task["point"] as! Int
+                            }
+                        }
+                        i += 1
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+    }
 }
 
 extension DashboardViewController: UITableViewDataSource, UITableViewDelegate {
@@ -43,6 +71,9 @@ extension DashboardViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "DashboardCell", for: indexPath) as! DashboardTableViewCell
+        userPointList = userPointList.sorted(by: { lUser, rUser -> Bool in
+            return lUser.point > rUser.point
+        })
         // カスタムセルにRealmの情報を反映
         cell.configure(user: userPointList[indexPath.row].name,
                        point: userPointList[indexPath.row].point)
