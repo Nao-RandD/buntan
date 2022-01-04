cef780fa7adac09067601ded1f23abfda9251832
Nao-RandD <nao.r.and.d@gmail.com>
2022-01-04 19:22:35 +0900

    AppStoreアップのために規定の画像、設定を修正

--- buntan/AppDelegate.swift
+++ buntan/AppDelegate.swift
@@ -8,6 +8,13 @@
 import UIKit
 import Firebase
 
+func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+        let output = items.map { "\($0)" }.joined(separator: separator)
+        Swift.print(output, terminator: terminator)
+    #endif
+}
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
