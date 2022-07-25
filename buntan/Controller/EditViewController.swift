f8c5ae793497110b990bff8dbde037defa307e1f
Nao-RandD <nao.r.and.d@gmail.com>
2022-07-25 13:16:57 +0900

    [add] EditViewControllerの中身を実装

--- buntan/Controller/EditViewController.swift
+++ buntan/Controller/EditViewController.swift
@@ -8,22 +8,56 @@
 import UIKit
 
 class EditViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var pointTextField: UITextField!
+
+    private var placeName: String = ""
+    private var placePoint: Int = 0
+    private let userDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        nameTextField.text = placeName
+        pointTextField.text = "\(placePoint)"
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-    */
 
+    @IBAction func tappedSaveButton(_ sender: Any) {
+        guard let name = nameTextField.text, !name.isEmpty else { return }
+        guard let point = pointTextField.text, !point.isEmpty else { return }
+        let group = self.userDefaults.object(forKey: "Group") as! String
+
+        let beforeTask = GroupTask(group: group, name: placeName, point: placePoint)
+        let afterTask = GroupTask(group: group, name: name, point: Int(point)!)
+
+        sendFirestore(before: beforeTask, after: afterTask)
+    }
+
+    /// 変更前のタスクをセットする
+    func setTaskItem(name: String, point: Int) {
+        placeName = name
+        placePoint = point
+    }
+}
+
+// MARK: - Private Func -
+
+extension EditViewController {
+    private func sendFirestore(before beforeTask: GroupTask, after afterTask: GroupTask) {
+        // afterTaskの内容にFirebaseを修正する
+        FirebaseManager.shared.editDocument(before: beforeTask,
+                                            after: afterTask,
+                                            completion: {
+            DispatchQueue.main.async {
+                self.nameTextField.text = ""
+                self.pointTextField.text = ""
+                self.dismiss(animated: true, completion: nil)
+            }
+        })
+    }
 }
