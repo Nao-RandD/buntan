2f6af74db4867c041be37ae34502c18e160889ce
Nao-RandD <nao.r.and.d@gmail.com>
2022-11-19 12:25:57 +0900

    [fix] ダッシュボードのNavigationBarのタイトルを修正

--- buntan/Controller/DashboardViewController.swift
+++ buntan/Controller/DashboardViewController.swift
@@ -23,7 +23,8 @@ class DashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.navigationItem.title = "ランキング"
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: "DashboardTableViewCell", bundle: nil),
