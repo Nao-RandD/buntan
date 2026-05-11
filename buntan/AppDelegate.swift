ca6d20ec149d5cd3620b329fd98d88b356b69e04
Nao-RandD <nao.r.and.d@gmail.com>
2026-05-11 21:09:49 +0900

    fix: Realmの依存解決周りでクラッシュする問題を修正

--- buntan/AppDelegate.swift
+++ buntan/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import FirebaseCore
 
 func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
     #if DEBUG
@@ -18,7 +17,6 @@ func print(_ items: Any..., separator: String = " ", terminator: String = "\n") 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
         return true
     }
 }
--- buntan/BuntanApp.swift
+++ buntan/BuntanApp.swift
@@ -1,9 +1,15 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct BuntanApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    @State private var appVM = AppViewModel()
+    @State private var appVM: AppViewModel
+
+    init() {
+        FirebaseApp.configure()
+        _appVM = State(wrappedValue: AppViewModel())
+    }
 
     var body: some Scene {
         WindowGroup {
